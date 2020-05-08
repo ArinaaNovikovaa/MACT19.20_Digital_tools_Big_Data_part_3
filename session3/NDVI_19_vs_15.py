09d8fbec53e501697c44338fbeaa92255d5e9777
Diego Pajarito <diegopajarito@gmail.com>
2020-05-08 09:11:07 -0500

    Updated resources for Session 3

---
+++ session3/NDVI_19_vs_15.py
@@ -0,0 +1,28 @@
+import qgis
+from qgis.analysis import QgsRasterCalculatorEntry, QgsRasterCalculator
+
+ndvi_before = 'NDVI_2015_int'
+ndvi_after = 'NDVI_2019_int'
+# Set this path before runing the script
+lyr_dif = '/home/diego/Documents/maps/Barcelona/images/NDVI_19_vs_15.tif'
+
+# Set up raster bands as variables
+ndvi_b = QgsRasterCalculatorEntry()
+ndvi_a = QgsRasterCalculatorEntry()
+# Set up the references
+ndvi_b.ref = 'bef@1'
+ndvi_a.ref = 'aft@1'
+# Set up raster values
+ndvi_b.raster = QgsProject.instance().mapLayersByName(ndvi_before)[0]
+ndvi_a.raster = QgsProject.instance().mapLayersByName(ndvi_after)[0]
+# Set up the band to read digital levels
+ndvi_b.bandNumber = 1
+ndvi_a.bandNumber = 1
+# Set entries for the Raster Calculator
+entries = []
+entries.append( ndvi_b )
+entries.append( ndvi_a )
+# Set the formula and execute it
+calc = QgsRasterCalculator( '"aft@1" - "bef@1"', lyr_dif, 'GTiff', ndvi_a.raster.extent(), ndvi_a.raster.width(), ndvi_a.raster.height(), entries )
+calc.processCalculation()
+print('Executed')
